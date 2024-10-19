4b3cdf0c108f52aba61c58521f5803f60c9a9340
Deepanshumelkani77 <deepumelkani1232@gmail.com>
2024-10-18 21:12:15 -0700

    create responsive animation

--- script.js
+++ script.js
@@ -65,17 +65,7 @@ slideimage();
 
 })
 
-//animation in link
 
-gsap.from(".link p",
-    {
-        y:-20,
-        opacity:0,
-        delay:1,
-        duration:0.5,
-        stagger:0.3
-    }
-)
 
 
 //sidebar navbar
@@ -155,12 +145,27 @@ slideimagee();
 
 
 
-
-
-
-
-
 //gsap animation
+
+//animation in link
+
+gsap.from(".link p",
+    {
+        y:-20,
+        opacity:0,
+        delay:1,
+        duration:0.5,
+        stagger:0.3
+    }
+)
+
+
+
+let mm=gsap.matchMedia();
+
+//for laptop
+mm.add("(min-width:1250px) and (max-width:1600px)",()=>{
+
 
 
 //scroll trigger for page 2
@@ -169,7 +174,7 @@ let tl=gsap.timeline({
     scrollTrigger:{
         trigger:".page2",
         scroller:"body",
-        start:"top 50%",
+        start:"top 45%",
         scrub:2,
         end:"top 0"
     }
@@ -199,7 +204,7 @@ let tl2=gsap.timeline({
     scrollTrigger:{
         trigger:".page3",
         scroller:"body",
-        start:"top 50%",
+        start:"top 45%",
         scrub:2,
         end:"top 0"
     }
@@ -238,7 +243,7 @@ let tl3=gsap.timeline({
     scrollTrigger:{
         trigger:".page4",
         scroller:"body",
-        start:"top 50%",
+        start:"top 45%",
         scrub:2,
         end:"top 0"
     }
@@ -261,7 +266,7 @@ let tl4=gsap.timeline({
     scrollTrigger:{
         trigger:".page6",
         scroller:"body",
-        start:"top 50%",
+        start:"top 45%",
         scrub:2,
         end:"top 0"
     }
@@ -297,3 +302,166 @@ tl4.from(".page6 .location .locat button",{
     opacity:0,
     duration:1
 })
+
+
+})
+
+
+
+
+
+
+mm.add("(min-width:580px) and (max-width:1249px)",()=>{
+
+
+
+
+let tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        start:"top 50%",
+        scrub:2,
+        end:"top 0",
+        markers:true
+    }
+})
+
+tl.from(".page2 .headding",{
+
+    x:-400,
+    duration:1,
+    opacity:0
+})
+
+tl.from(".page2 .cloths .c1",{
+    x:-800,
+    duration:1
+})
+
+tl.from(".page2 .cloths .c3",{
+    x:800,
+    duration:1
+},"-=1")
+
+
+//scroll trigger page3
+
+let tl2=gsap.timeline({
+    scrollTrigger:{
+        trigger:".page3",
+        scroller:"body",
+        start:"top 20%",
+        scrub:2,
+        end:"top 0"
+    }
+})
+
+tl2.from(".page3 .contain .text h1",{
+
+    x:-400,
+    opacity:0,
+    duration:0.5
+})
+tl2.from(".page3 .contain .text p",{
+
+    x:-400,
+    opacity:0,
+    duration:0.5
+},"-=0.5")
+tl2.from(".page3 .contain .text button",{
+
+   
+    opacity:0,
+    duration:0.5
+})
+
+tl2.from(".page3 .contain  .image img",{
+
+   
+    opacity:0,
+    duration:0.5
+},"-=0.5")
+
+
+
+//scroll trigger page4
+let tl3=gsap.timeline({
+    scrollTrigger:{
+        trigger:".page4",
+        scroller:"body",
+        start:"top 20%",
+        scrub:2,
+        end:"top 0"
+    }
+})
+
+tl3.from(".page4 .contain .men",{
+    scale:0,
+    duration:0.5
+})
+tl3.from(".page4 .contain .women",{
+    scale:0,
+    duration:0.5
+},"-=0.5")
+
+
+
+//scroll trigger page6
+
+let tl4=gsap.timeline({
+    scrollTrigger:{
+        trigger:".page6",
+        scroller:"body",
+        start:"top 20%",
+        scrub:2,
+        end:"top 0"
+    }
+})
+
+tl4.from(".page6 .cardd1",{
+    x:-500,
+    duration:1
+
+})
+tl4.from(".page6 .cardd3",{
+    x:500,
+    duration:1
+
+},"-=1")
+
+tl4.from(".page6 .location .store img",{
+    delay:1,
+    opacity:0,
+    duration:1
+})
+
+
+tl4.from(".page6 .location .locat h1",{
+    x:800,
+    duration:1
+},"-=1")
+tl4.from(".page6 .location .locat h4",{
+    x:800,
+    duration:1
+},'-=1')
+tl4.from(".page6 .location .locat button",{
+    opacity:0,
+    duration:1
+})
+
+
+
+})
+
+
+
+
+mm.add("(min-width:0px) and (max-width:427px)",()=>{
+
+
+
+
+
+})
+
